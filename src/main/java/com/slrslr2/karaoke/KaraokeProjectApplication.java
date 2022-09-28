840000a660e17bdade3ed0c1e39b0c6b3baf8ec4
slrslrr2 <slrslrr1@gmail.com>
2022-09-28 20:42:40 +0900

    firstCommit

---
+++ src/main/java/com/slrslr2/karaoke/KaraokeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.slrslr2.karaoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slrslr2/karaoke/KaraokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slrslr2.karaoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
